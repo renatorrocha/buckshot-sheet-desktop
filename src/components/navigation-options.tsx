04673d614b7cffb0a9f3411e7fb44ba88d9a2a22
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-11-03 16:35:09 -0300

    wip: navigation

---
+++ src/components/navigation-options.tsx
@@ -0,0 +1,9 @@
+export default function NavigationOptions() {
+    return (
+        <section className="flex flex-col mt-4 text-lg text-center uppercase sm:gap-2 sm:text-3xl">
+            <p>start</p>
+            <p>credits</p>
+            <p>exit</p>
+        </section>
+    );
+}
