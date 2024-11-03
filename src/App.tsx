f9ff0870b46d03840b9adc58e8eae40c610a13c9
renatorrodrigues <renatorrodrigues2002@gmail.com>
2024-11-03 16:20:23 -0300

    wip: homePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,19 @@
 export default function App() {
-    return <div className="text-red-800 font-bold">App</div>;
+    return (
+        <main className="h-screen antialiased text-white bg-brand-black font-averia">
+            <div className="flex flex-col items-center justify-center h-full gap-2">
+                <h1 className="text-3xl font-black text-center text-white uppercase sm:text-6xl h-fit text-stroke-2 ">
+                    <p className="inline-block scale-y-[2] h-fit mb-2 sm:mb-4">
+                        buckshot
+                    </p>
+                    <p className="scale-x-[1.05]">roulette</p>
+                    <p className="scale-x-125">sheet</p>
+                </h1>
+
+                <p className="w-32 text-sm text-center sm:w-64 sm:text-2xl">
+                    A sheet for Mike Klubnika's game
+                </p>
+            </div>
+        </main>
+    );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                averia: ['"Averia Serif Libre"', "serif"],
+            },
+            textStroke: {
+                1: "1px",
+                2: "2px",
+            },
+            colors: {
+                white: "#dbdbd9",
+                brand: {
+                    black: "#010000",
+                },
+            },
+        },
     },
-    plugins: [],
+    plugins: [
+        ({ addUtilities }) => {
+            addUtilities({
+                ".text-stroke-1": {
+                    "-webkit-text-stroke": "1px #000",
+                },
+                ".text-stroke-2": {
+                    "-webkit-text-stroke": "2px #000",
+                },
+            });
+        },
+    ],
 };
